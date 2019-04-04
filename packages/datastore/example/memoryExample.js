e9c7b0d60d48d4005eb2e09bfea2e3b4e4df8934
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-04-03 20:30:02 -0500

    Add Example Files
    This commit adds example files for reference when using the different apis. Both examples require the datastore to be built (should happen automatically with a yarn install).
    
    The redis example requires a redis server to be running locally on the default port 6379.

---
+++ packages/datastore/example/memoryExample.js
@@ -0,0 +1,44 @@
+const MemoryApi = require('../dist/memoryApi').default;
+
+const api = new MemoryApi('test');
+
+// Memory api is synchronous
+try {
+  const browsePayload = api.browse();
+  console.log(browsePayload);
+  const addPayload = api.add({ test: 'add1' });
+  console.log(addPayload);
+  const readPayload = api.read(addPayload.id);
+  console.log(readPayload);
+  const editPayload = api.edit(readPayload.id, { test: 'add2' });
+  console.log(editPayload);
+  const deletePayload = api.delete(editPayload.id);
+  console.log(deletePayload);
+} catch (err) {
+  console.log(err);
+}
+
+// Memory api can also support promise style usage (first call needs be wrapped though)
+// Promise.resolve(api.browse())
+//   .then(payload => {
+//     console.log(payload);
+//     return api.add({ test: 'add1' });
+//   })
+//   .then(payload => {
+//     console.log(payload);
+//     return api.read(payload.id);
+//   })
+//   .then(payload => {
+//     console.log(payload);
+//     return api.edit(payload.id, { test: 'add2' });
+//   })
+//   .then(payload => {
+//     console.log(payload);
+//     return api.delete(payload.id);
+//   })
+//   .then(payload => {
+//     console.log(payload);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
