c3ee962c0b6406a08490a49ab9e825408a742029
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-04-07 23:54:57 -0500

    Add Top Level Export
    This commit adds a top level export so the stores can be used in external packages.

---
+++ packages/datastore/src/index.js
@@ -0,0 +1,10 @@
+import MemoryStore from './store/memoryStore';
+import RedisStore from './store/redisStore';
+
+import Datasources from './utils/datasources';
+
+export default {
+  MemoryStore,
+  RedisStore,
+  Datasources,
+};
