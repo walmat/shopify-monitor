196035238bf9b155de7afbe47c423bb38818c49d
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-04-16 16:32:12 -0500

    Export GraphQL Types (#8)
    This commit adds the last part forgotten in #7 -- exporting the graphql types from the main index file.

--- packages/structures/src/index.js
+++ packages/structures/src/index.js
@@ -4,6 +4,16 @@ import proxyDefn from './definitions/proxy';
 import settingsDefn from './definitions/settings';
 import siteDefn from './definitions/site';
 
+import {
+  KeywordInfoType,
+  KeywordInfoDataInputType,
+  KeywordInfoStringInputType,
+} from './graphql/keywordInfo';
+import { MonitorInfoType, MonitorInfoInputType } from './graphql/monitorInfo';
+import { ProxyType, ProxyDataInputType, ProxyStringInputType } from './graphql/proxy';
+import { SettingsType, SettingsInputType } from './graphql/settings';
+import { SiteType, SiteInputType } from './graphql/site';
+
 import keywordInfoState from './initialStates/keywordInfo';
 import monitorInfoState from './initialStates/monitorInfo';
 import proxyState from './initialStates/proxy';
@@ -14,7 +24,21 @@ import buildKeywordInfo from './utils/buildKeywordInfo';
 import buildProxyInfo from './utils/buildProxy';
 
 const definitions = { keywordInfoDefn, monitorInfoDefn, proxyDefn, settingsDefn, siteDefn };
+const graphql = {
+  KeywordInfoType,
+  KeywordInfoDataInputType,
+  KeywordInfoStringInputType,
+  MonitorInfoType,
+  MonitorInfoInputType,
+  ProxyType,
+  ProxyDataInputType,
+  ProxyStringInputType,
+  SettingsType,
+  SettingsInputType,
+  SiteType,
+  SiteInputType,
+};
 const initialStates = { keywordInfoState, monitorInfoState, proxyState, settingsState, siteState };
 const utils = { buildKeywordInfo, buildProxyInfo };
 
-export { definitions, initialStates, utils };
+export { definitions, graphql, initialStates, utils };
