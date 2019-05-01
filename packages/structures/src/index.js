d3c9269a7a048b87d3425c05bb080a5b35314356
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-05-01 00:11:53 -0500

    Export New Definitions
    This commit updates the top level export file to include the new definitions. These will now be available for use in other packages.

--- packages/structures/src/index.js
+++ packages/structures/src/index.js
@@ -1,8 +1,11 @@
 import keywordInfoDefn from './definitions/keywordInfo';
 import monitorInfoDefn from './definitions/monitorInfo';
+import productDefn from './definitions/product';
 import proxyDefn from './definitions/proxy';
 import settingsDefn from './definitions/settings';
 import siteDefn from './definitions/site';
+import variantDefn from './definitions/variant';
+import webhookGroupDefn from './definitions/webhookGroup';
 
 import {
   KeywordInfoType,
@@ -10,26 +13,43 @@ import {
   KeywordInfoStringInputType,
 } from './graphql/keywordInfo';
 import { MonitorInfoType, MonitorInfoInputType } from './graphql/monitorInfo';
+import { ProductType, ProductInputType } from './graphql/product';
 import { ProxyType, ProxyDataInputType, ProxyStringInputType } from './graphql/proxy';
 import { SettingsType, SettingsInputType } from './graphql/settings';
 import { SiteType, SiteInputType } from './graphql/site';
+import { VariantType, VariantInputType } from './graphql/variant';
+import { WebhookGroupType, WebhookGroupInputType } from './graphql/webhookGroup';
 
 import keywordInfoState from './initialStates/keywordInfo';
 import monitorInfoState from './initialStates/monitorInfo';
+import productState from './initialStates/product';
 import proxyState from './initialStates/proxy';
 import settingsState from './initialStates/settings';
 import siteState from './initialStates/site';
+import variantState from './initialStates/variant';
+import webhookGroupState from './initialStates/webhookGroup';
 
 import buildKeywordInfo from './utils/buildKeywordInfo';
 import buildProxyInfo from './utils/buildProxy';
 
-const definitions = { keywordInfoDefn, monitorInfoDefn, proxyDefn, settingsDefn, siteDefn };
+const definitions = {
+  keywordInfoDefn,
+  monitorInfoDefn,
+  productDefn,
+  proxyDefn,
+  settingsDefn,
+  siteDefn,
+  variantDefn,
+  webhookGroupDefn,
+};
 const graphql = {
   KeywordInfoType,
   KeywordInfoDataInputType,
   KeywordInfoStringInputType,
   MonitorInfoType,
   MonitorInfoInputType,
+  ProductType,
+  ProductInputType,
   ProxyType,
   ProxyDataInputType,
   ProxyStringInputType,
@@ -37,8 +57,21 @@ const graphql = {
   SettingsInputType,
   SiteType,
   SiteInputType,
+  VariantType,
+  VariantInputType,
+  WebhookGroupType,
+  WebhookGroupInputType,
 };
-const initialStates = { keywordInfoState, monitorInfoState, proxyState, settingsState, siteState };
+const initialStates = {
+  keywordInfoState,
+  monitorInfoState,
+  productState,
+  proxyState,
+  settingsState,
+  siteState,
+  variantState,
+  webhookGroupState,
+};
 const utils = { buildKeywordInfo, buildProxyInfo };
 
 export { definitions, graphql, initialStates, utils };
