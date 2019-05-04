581cbb2a02f88e3c2b07709e916203eec9d260a5
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-05-03 19:30:52 -0500

    Add Split Process Manager
    This commit adds a subclass of SplitContextManager for child processes. 
    
    In order to fully implement this, a ContextTransformer class is added to pass events sent between contexts to the monitor instance.
    
    The base class is defined and a specific process implementation is defined and used in the SplitProcessManager.

---
+++ packages/monitor/src/managers/splitProcessManager.js
@@ -0,0 +1,50 @@
+const childProcess = require('child_process');
+const path = require('path');
+
+const SplitContextManager = require('./splitContextManager');
+
+class ProcessContext {
+  constructor(mId, data, proxy) {
+    this.id = mId;
+    this.monitorIds = [data.id];
+    this.site = data.site;
+    this.proxy = proxy;
+    this._name = 'Child Process';
+    this._target = 'child';
+    this._child = childProcess.fork(path.resolve(__dirname, '../runnerScripts/process.js'));
+
+    this.isExitPayload = () => true;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get target() {
+    return this._target;
+  }
+
+  send(payload) {
+    this._child.send(payload);
+  }
+
+  on(channel, handler) {
+    this._child.on(channel, handler);
+  }
+
+  removeListener(channel, handler) {
+    this._child.removeListener(channel, handler);
+  }
+
+  kill() {
+    this._child.kill();
+  }
+}
+
+class SplitProcessManager extends SplitContextManager {
+  constructor() {
+    super(ProcessContext);
+  }
+}
+
+module.exports = SplitProcessManager;
---
+++ packages/monitor/src/runnerScripts/process.js
@@ -0,0 +1,24 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["send"] }] */
+const MonitorContextTransformer = require('./base');
+
+class MonitorProcessTransformer extends MonitorContextTransformer {
+  constructor(contextName = 'child') {
+    super(contextName);
+  }
+
+  wireErrors(errorTransformer) {
+    process.on('uncaughtException', errorTransformer.bind(this));
+    process.on('unhandledRejection', errorTransformer.bind(this));
+  }
+
+  send(payload) {
+    process.send(payload);
+  }
+
+  receive(handler) {
+    process.on('message', ({ target, event, args }) => handler.call(this, target, event, args));
+  }
+}
+
+const transformer = new MonitorProcessTransformer();
+transformer.start();
